cd75831452efd9b98702fb09d0a9a66ca941b7d9
chibachaseygaming-arch <chibachaseygaming@gmail.com>
2026-09-21 20:38:19 +1000

    Prepare static online game deployment

---
+++ build.js
@@ -0,0 +1,6 @@
+const fs = require('node:fs');
+fs.mkdirSync('dist/vendor', { recursive: true });
+for (const file of ['index.html', 'style.css', 'game.js', 'progression.js', 'vendor/three.min.js']) {
+  fs.copyFileSync(file, `dist/${file}`);
+}
+console.log('Static game ready in dist/');
